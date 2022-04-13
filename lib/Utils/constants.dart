d8b590877db5b98a766b26e6e22d7fd155e6dceb
lennie-ein <lenniemaina05@gmail.com>
2022-04-13 15:40:05 +0300

    color constants

---
+++ lib/Utils/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+mixin HealthyColors{
+  static Color HealthyNavy = Color(0xFF083248);
+  static Color HealthyBg = Color(0xFF557B83);
+  static Color HealthyTeal = Color(0xFF39AEA9);
+  static Color HealthyLeafy = Color(0xFFA2D5AB);
+  static Color HealthyCream = Color(0xFFE5EFC1);
+  static Color HealthyBeige = Color(0xFFDBA858);
+
+
+}
