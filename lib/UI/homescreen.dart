0e3548d8d30bc0b64be2979fa110ff1ad66cb957
lennie-ein <lenniemaina05@gmail.com>
2022-04-21 22:57:23 +0300

    Bottom Navigation bar added

--- lib/UI/homescreen.dart
+++ lib/UI/homescreen.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:healthy_pacer/Utils/constants.dart';
+import 'package:motion_tab_bar_v2/motion-tab-bar.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -7,9 +10,48 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin{
+
+  TabController? _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+      initialIndex: 1,
+      length: 3,
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _tabController!.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: MotionTabBar(
+        initialSelectedTab: "Dashboard",
+        labels: const ["Events", "Dashboard", 'Leader Board'],
+        icons: [
+          CupertinoIcons.circle_grid_hex_fill,
+          CupertinoIcons.home,
+          CupertinoIcons.square_favorites
+        ],
+        onTabItemSelected: (int value) {
+          setState(() {
+            _tabController!.index = value;
+          });
+        },
+        tabBarColor: HealthyColors.HealthyLeafy,
+        tabSelectedColor: HealthyColors.HealthyNavy,
+        tabIconColor: HealthyColors.HealthyNavy,
+        textStyle: TextStyle(
+            color: HealthyColors.HealthyNavy, fontWeight: FontWeight.bold),
+      ),
+    );
   }
 }
